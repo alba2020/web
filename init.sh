1492f67911ccd2ee724453cdf5fc8845c680a687
Alba2020 <alba2020@gmail.com>
2018-06-10 03:53:59 +0300

    change init.sh

--- init.sh
+++ init.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+sudo rm /etc/nginx/sites-enabled/default
+
 sudo ln -s -f /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 
 sudo ln -s -f /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+sudo /etc/init.d/gunicorn restart
 
 # sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 # sudo /etc/init.d/nginx restart
