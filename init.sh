4728d1d3a573f36e571e90932122a28b58294f52
Alba2020 <alba2020@gmail.com>
2018-06-10 03:59:09 +0300

    change gunicorn start

--- init.sh
+++ init.sh
@@ -6,7 +6,8 @@ sudo ln -s -f /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 
 sudo ln -s -f /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
-sudo /etc/init.d/gunicorn restart
+# sudo /etc/init.d/gunicorn restart
+/home/box/web/gu_start.sh
 
 # sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 # sudo /etc/init.d/nginx restart
